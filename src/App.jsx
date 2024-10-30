9531e3e20bd919190db4776f559430a20e53cdee
Benny-Nwaro <baroh3767@aol.com>
2024-10-30 22:14:11 +0100

    opend first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Footer from "./components/Footer";
+import "./assets/main.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+
+function App() {
+  // const NFTID = "rrkah-fqaaa-aaaaa-aaaaq-cai";
+
+  return (
+    <div className="App">
+      <Header />
+      {/* <Minter /> */}
+      {/* <Item id={NFTID}/> */}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import homeImage from "../assets/home-img.png";
+
+
+export default function Home() {
+  return (
+    <img className="bottom-space" src={homeImage} />
+
+  )
+}
