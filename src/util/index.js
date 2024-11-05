f94572ba33bb0aef5cca6234874305eccc044df8
Benny-Nwaro <baroh3767@aol.com>
2024-11-05 11:52:36 +0100

    fixing cors issues

--- src/util/index.js
+++ src/util/index.js
@@ -1,14 +1,10 @@
-import {jwtDecode} from "jwt-decode";
 
 
 const isDevelopment = window.location.hostname.includes("localhost");
 
 const getServer = () =>{
-   return isDevelopment ? "https://vend-app-server-is9c.vercel.app" : "https://vend-app-server-is9c.vercel.app";
+   return isDevelopment ? "https://vend-app-server-7f6h.vercel.app" : "https://vend-app-server-7f6h.vercel.app"; 
 }
-const decodeUser = () => {
-   const token = localStorage.getItem("token");
-   return jwtDecode(token);
- };
+
  
- export { getServer, decodeUser };
+ export { getServer };
